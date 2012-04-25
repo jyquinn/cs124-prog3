b20f8e6915a7c3bd47d7fc6391cc7d717d64a834
Nicholas Stanford <npstanford@gmail.com>
2012-04-25 13:28:47 -0400

    added input from file and made test.txt file

--- prog3/src/Main.java
+++ prog3/src/Main.java
@@ -1,17 +1,33 @@
 import java.io.*;
 import java.util.Random;
+import org.apache.commons.io.FileUtils;
 
 
 public class Main {
 
 	public static int MAX_ITER = 25000;
+	public static int FILE_SIZE = 100;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		File text_file = new File(args[0]);
+		String text = "";
+		try {
+			text = FileUtils.readFileToString(text_file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String[] numStrings = text.split("\\n");
+		long[] nums = new long[FILE_SIZE]; 
+		
+		int i=0;
+		for(i=0; i<nums.length; i++){
+			nums[i] = Integer.parseInt(numStrings[i]);		
+		}
+		
 	}
 	
 	public long[] genRandomExperiment(int n, int max_int){
