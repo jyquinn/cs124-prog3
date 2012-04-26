d2d0731b69c15b428b0294287c7edd3ca9b1d57a
Nicholas Stanford <npstanford@gmail.com>
2012-04-25 20:13:15 -0400

    added scripting for experiments, now testing

--- prog3/src/Main.java
+++ prog3/src/Main.java
@@ -4,9 +4,11 @@ import org.apache.commons.io.FileUtils;
 
 
 public class Main {
-
+	
+	public static long MAX_SIZE = (long)Math.pow(10, 12);
 	public static int MAX_ITER = 25000;
 	public static int FILE_SIZE = 100;
+	public static int NUM_TESTS = 50;
 
 	/**
 	 * @param args
@@ -28,15 +30,20 @@ public class Main {
 			nums[i] = Integer.parseInt(numStrings[i]);		
 		}
 		
-		testHeap();
+		mainScript();
 		
 	}
 	
-	public long[] genRandomExperiment(int n, int max_int){
+	public static long[] genRandomExperiment(int n){
 		Random rand = new Random();
 		long[] sequence = new long[n];
-		for (int i = 0; i < n; i++)
-			sequence[i] = rand.nextInt(max_int);
+		long s = 0;
+		int i = 0;
+		for (i = 0; i < n; i++) {
+			s = rand.nextLong()%MAX_SIZE;
+			if (s<0){ s=s*-1; }
+			sequence[i] = s;
+		}
 		return sequence;
 	}
 	
@@ -65,4 +72,85 @@ public class Main {
 	}
 	
 
+	public static void testKK(){
+		long[] nums = new long[]{5,7,10,8,6};
+		KK kk = new KK(nums);
+		System.out.println(kk.getResidue());
+	} 
+	
+	
+	public static void mainScript(){
+		int i;
+		
+		long[] kk_res        = new long[NUM_TESTS];
+		long[] stand_rr_res  = new long[NUM_TESTS];
+		long[] stand_hc_res  = new long[NUM_TESTS];
+		long[] stand_sa_res  = new long[NUM_TESTS];
+		long[] pre_rr_res    = new long[NUM_TESTS];
+		long[] pre_hc_res    = new long[NUM_TESTS];
+		long[] pre_sa_res    = new long[NUM_TESTS];
+		
+		long[] kk_time       = new long[NUM_TESTS];
+		long[] stand_rr_time = new long[NUM_TESTS];
+		long[] stand_hc_time = new long[NUM_TESTS];
+		long[] stand_sa_time = new long[NUM_TESTS];
+		long[] pre_rr_time   = new long[NUM_TESTS];
+		long[] pre_hc_time   = new long[NUM_TESTS];
+		long[] pre_sa_time   = new long[NUM_TESTS];
+		
+		long time_start = 0;
+		long time_finish = 0;
+		
+		for (i=0; i< NUM_TESTS; i++){
+			//long[] exp = genRandomExperiment(FILE_SIZE);
+			long[] exp = new long[]{5,7,10,8,6};
+			KK kk_exp = new KK(exp);
+			StandardSolution stand_exp = new StandardSolution(exp);
+			PrePartitionSolution pre_exp = new PrePartitionSolution(exp);
+			
+			time_start       = System.currentTimeMillis();
+			kk_res[i]        = kk_exp.getResidue();	
+			time_finish      = System.currentTimeMillis();
+			kk_time [i]      = time_finish-time_start;
+			
+			time_start       = System.currentTimeMillis();
+			stand_rr_res[i]  = stand_exp.runRandom().getResidue();
+			time_finish      = System.currentTimeMillis();
+			stand_rr_time[i] = time_finish-time_start;
+			
+			time_start       = System.currentTimeMillis();
+			stand_hc_res[i]  = stand_exp.runHillClimbing().getResidue();
+			time_finish      = System.currentTimeMillis();
+			stand_hc_time[i] = time_finish-time_start;			
+			
+			time_start       = System.currentTimeMillis();
+			stand_sa_res[i]  = stand_exp.runSimulatedAnnealing().getResidue();	
+			time_finish      = System.currentTimeMillis();
+			stand_sa_time[i] = time_finish-time_start;				
+			
+			time_start       = System.currentTimeMillis();			
+			pre_rr_res[i]    = pre_exp.runRandom().getResidue();
+			time_finish      = System.currentTimeMillis();
+			pre_rr_time[i]   = time_finish-time_start;				
+			
+			time_start       = System.currentTimeMillis();			
+			pre_hc_res[i]    = pre_exp.runHillClimbing().getResidue();
+			time_finish      = System.currentTimeMillis();
+			pre_hc_time[i]   = time_finish-time_start;				
+			
+			time_start       = System.currentTimeMillis();			
+			pre_sa_res[i]    = pre_exp.runSimulatedAnnealing().getResidue();
+			time_finish      = System.currentTimeMillis();
+			pre_sa_time[i]   = time_finish-time_start;		
+			
+			System.out.println(kk_res[i] + " " + kk_time[i] + "   " 
+					+ stand_rr_res[i] + " " + stand_rr_time[i] + "    " 
+					+ stand_hc_res[i] + " " + stand_hc_time[i] + "    " 
+					+ stand_sa_res[i] + " " + stand_sa_time[i] + "    " 	
+					+ pre_rr_res[i] + " " + pre_rr_time[i] + "    " 
+					+ pre_hc_res[i] + " " + pre_hc_time[i] + "    "
+					+ pre_sa_res[i] + " " + pre_sa_time[i] + "    " 
+					);
+		}
+	}
 }
