ae75e5f1323b491e98eef4d11e487acbea4c1f84
Jacqueline Quinn <jyquinn@gmail.com>
2012-04-25 21:02:11 -0400

    altered kk so getresidue can be called multiple times

--- prog3/src/KK.java
+++ prog3/src/KK.java
@@ -1,15 +1,22 @@
 
 public class KK {
 	private MaxHeap nums;
+	private long residue;
 	
 	public KK(long[] nums_init){
 		nums = new MaxHeap(nums_init);
+		residue = calcResidue();
 	}
 	public KK(MaxHeap nums_heaped){
 		nums = nums_heaped;
+		residue = calcResidue();
 	}
 	
 	public long getResidue(){
+		return residue;
+	}
+	
+	private long calcResidue(){
 		while (nums.getSize() > 1){
 			nums.insert(Math.abs(nums.extractMax() - nums.extractMax()));
 		}
